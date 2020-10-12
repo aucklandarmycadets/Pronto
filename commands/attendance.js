ff38155e0d467c0c29f32900a222244db2289aac
James <jqmesnz@gmail.com>
2020-10-13 11:08:07 +1300

    Added attendance register update feature

--- commands/attendance.js
+++ commands/attendance.js
@@ -6,13 +6,21 @@ const { cmds: { attendance } } = require('../cmds');
 const { cmdError, sendMsg, dmError, debugError } = require('../modules');
 
 module.exports = attendance;
-module.exports.execute = (msg, args) => {
+module.exports.execute = async (msg, args) => {
 	'use strict';
 
 	const { bot } = require('../pronto.js');
 	const memberRoles = msg.member.roles.cache;
 
-	if (args.length === 0) return cmdError(msg, 'You must enter a message.', attendance.error);
+	try {
+		if (args.length === 0) throw '';
+
+		if (args[0] === 'update' && args.length === 1) throw '';
+	}
+
+	catch (error) {
+		return cmdError(msg, 'You must enter a message.', attendance.error);
+	}
 
 	msg.delete().catch(error => debugError(error, `Error deleting message in ${msg.channel}.`, 'Message', msg.content));
 
@@ -28,71 +36,116 @@ module.exports.execute = (msg, args) => {
 
 	const successEmojiObj = msg.guild.emojis.cache.find(emoji => emoji.name === successEmoji);
 	const errorEmojiObj = msg.guild.emojis.cache.find(emoji => emoji.name === errorEmoji);
-	const register = args.join(' ');
 
-	const attendanceEmbed = new Discord.MessageEmbed()
-		.setColor(formationColour)
-		.setAuthor(formationName, msg.guild.iconURL())
-		.setDescription(register)
-		.setFooter('Use the reactions below to confirm or cancel.');
+	if (args[0] === 'update') {
+		args.splice(0, 1);
 
-	msg.author.send(attendanceEmbed)
-		.then(dm => {
-			dm.react(successEmojiObj)
-				.then(() => dm.react(errorEmojiObj))
-				.catch(error => debugError(error, 'Error reacting to message in DMs.'));
+		if (args.length === 0) return cmdError(msg, 'You must enter a message.', attendance.error);
 
-			const filter = (reaction) => reaction.emoji.name === successEmoji || reaction.emoji.name === errorEmoji;
+		const filterBy = message => {
+			try {
+				return message.embeds[0].author.name.includes(formationName);
+			}
 
-			const collector = dm.createReactionCollector(filter, { time: 90000, dispose: true });
+			catch {}
+		};
 
-			collector.on('collect', (reaction, user) => {
-				if (!user.bot) {
-					const updateEmbed = new Discord.MessageEmbed()
-						.setAuthor(bot.user.tag, bot.user.avatarURL())
-						.setColor(colours.error)
-						.setDescription('**Cancelled.**')
-						.setFooter(dateFormat(dateOutput));
+		const attendanceChannel = bot.channels.cache.get(attendanceID);
 
-					if (reaction.emoji.name === successEmoji) {
-						const attendanceChannel = bot.channels.cache.get(attendanceID);
+		let chnlMsg, attMsg;
 
-						updateEmbed.setColor(colours.success);
-						updateEmbed.setDescription('**Confirmed.**');
-
-						attendanceEmbed.setFooter(dateFormat(dateOutput));
-
-						sendMsg(attendanceChannel, attendanceEmbed);
-						sendMsg(msg.channel, attendanceEmbed);
-					}
-
-					dm.edit(updateEmbed);
-					collector.stop();
-				}
+		await msg.channel.messages.fetch()
+			.then(messages => {
+				chnlMsg = messages.filter(filterBy).first();
 			});
 
-			collector.on('end', async (collected, reason) => {
-				const userReactions = dm.reactions.cache.filter(reaction => reaction.users.cache.has(bot.user.id));
-				try {
-					for (const reaction of userReactions.values()) {
-						await reaction.users.remove(bot.user.id);
-					}
-				}
-
-				catch (error) {
-					debugError(error, 'Error removing reactions from message in DMs.');
-				}
-
-				if (reason === 'time') {
-					dm.delete().catch(error => debugError(error, 'Error deleting message in DMs.'));
-
-					const timeEmbed = new Discord.MessageEmbed()
-						.setColor(colours.error)
-						.setAuthor(bot.user.tag, bot.user.avatarURL())
-						.setDescription('Timed out. Please action the command again.');
-					sendMsg(msg.author, timeEmbed, true);
-				}
+		await attendanceChannel.messages.fetch()
+			.then(messages => {
+				attMsg = messages.filter(filterBy).first();
 			});
-		})
-		.catch(error => dmError(msg, error));
+
+		createRegister(chnlMsg, attMsg);
+	}
+
+	else createRegister();
+
+	function createRegister(chnlMsg, attMsg) {
+		const register = args.join(' ');
+
+		const attendanceEmbed = new Discord.MessageEmbed()
+			.setColor(formationColour)
+			.setAuthor(formationName, msg.guild.iconURL())
+			.setDescription(register)
+			.setFooter('Use the reactions below to confirm or cancel.');
+
+		if (chnlMsg) attendanceEmbed.setAuthor(`${formationName} (Update)`, msg.guild.iconURL())
+
+		msg.author.send(attendanceEmbed)
+			.then(dm => {
+				dm.react(successEmojiObj)
+					.then(() => dm.react(errorEmojiObj))
+					.catch(error => debugError(error, 'Error reacting to message in DMs.'));
+
+				const filter = (reaction) => reaction.emoji.name === successEmoji || reaction.emoji.name === errorEmoji;
+
+				const collector = dm.createReactionCollector(filter, { dispose: true });
+
+				collector.on('collect', (reaction, user) => {
+					if (!user.bot) {
+						const updateEmbed = new Discord.MessageEmbed()
+							.setAuthor(bot.user.tag, bot.user.avatarURL())
+							.setColor(colours.error)
+							.setDescription('**Cancelled.**')
+							.setFooter(dateFormat(dateOutput));
+
+						if (reaction.emoji.name === successEmoji) {
+							updateEmbed.setColor(colours.success);
+							updateEmbed.setDescription('**Confirmed.**');
+
+							attendanceEmbed.setAuthor(`${formationName} (${msg.member.displayName})`, msg.guild.iconURL());
+							attendanceEmbed.setFooter(dateFormat(dateOutput));
+
+							if (chnlMsg) {
+								chnlMsg.edit(attendanceEmbed);
+								attMsg.edit(attendanceEmbed);
+							}
+
+							else {
+								const attendanceChannel = bot.channels.cache.get(attendanceID);
+
+								sendMsg(attendanceChannel, attendanceEmbed);
+								sendMsg(msg.channel, attendanceEmbed);
+							}
+						}
+
+						dm.edit(updateEmbed);
+						collector.stop();
+					}
+				});
+
+				collector.on('end', async (collected, reason) => {
+					const userReactions = dm.reactions.cache.filter(reaction => reaction.users.cache.has(bot.user.id));
+					try {
+						for (const reaction of userReactions.values()) {
+							await reaction.users.remove(bot.user.id);
+						}
+					}
+
+					catch (error) {
+						debugError(error, 'Error removing reactions from message in DMs.');
+					}
+
+					if (reason === 'time') {
+						dm.delete().catch(error => debugError(error, 'Error deleting message in DMs.'));
+
+						const timeEmbed = new Discord.MessageEmbed()
+							.setColor(colours.error)
+							.setAuthor(bot.user.tag, bot.user.avatarURL())
+							.setDescription('Timed out. Please action the command again.');
+						sendMsg(msg.author, timeEmbed, true);
+					}
+				});
+			})
+			.catch(error => dmError(msg, error));
+	}
 };
