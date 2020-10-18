7d79fb77cf58df4c150e21e2e5f3dafffd11fedd
James <jqmesnz@gmail.com>
2020-10-18 19:10:35 +1300

    Split messages if message length is greater than 2000 chars

--- commands/evaluate.js
+++ commands/evaluate.js
@@ -6,7 +6,7 @@ module.exports.execute = (msg, args) => {
 	'use strict';
 
 	const { bot } = require('../pronto');
-	
+
 	if (args.length === 0) return cmdError(msg, 'You must enter something to evaluate.', evaluate.error);
 
 	const code = args.join(' ');
@@ -16,8 +16,24 @@ module.exports.execute = (msg, args) => {
 
 		if (typeof evaled !== 'string') evaled = require('util').inspect(evaled);
 
-		sendMsg(msg.channel, `\`\`\`js\n${evaled}\`\`\``);
+		const output = [evaled];
+		let string = last(output);
+
+		while (string.length > 2000) {
+			const breakAt = string.lastIndexOf(',', 1990) + 1;
+
+			output.pop();
+			splitOn(string, breakAt).forEach(str => output.push(str));
+
+			string = last(output);
+		}
+
+		output.forEach(str => sendMsg(msg.channel, `\`\`\`js\n${str}\`\`\``));
 	}
 
 	catch (error) { debugError(error, `Error evaluating \`${code}\``); }
 };
+
+const last = arr => { return arr[arr.length - 1]; };
+
+const splitOn = (slicable, ...indices) => [0, ...indices].map((n, i, m) => slicable.slice(n, m[i + 1]));
