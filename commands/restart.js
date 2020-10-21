bc607a476a33a7e7862ca878fc570eab8e9087b3
James <jqmesnz@gmail.com>
2020-10-21 14:23:52 +1300

    Send restarting embed on all exit events, adjust !restart accordingly

--- commands/restart.js
+++ commands/restart.js
@@ -1,29 +1,14 @@
-const Discord = require('discord.js');
-
-const { ids: { serverID, devID }, emojis: { successEmoji }, colours } = require('../config');
+const { ids: { serverID }, emojis: { successEmoji } } = require('../config');
 const { cmds: { restart } } = require('../cmds');
-const { dtg, formatAge, debugError } = require('../modules');
+const { debugError } = require('../modules');
 
 module.exports = restart;
 module.exports.execute = msg => {
-	'use strict';
-
-	const { bot, version } = require('../pronto.js');
-
-	const dev = bot.users.cache.get(devID);
+	const { bot } = require('../pronto');
 	const server = bot.guilds.cache.get(serverID);
 	const successEmojiObj = server.emojis.cache.find(emoji => emoji.name === successEmoji);
 
 	msg.react(successEmojiObj).catch(error => debugError(error, `Error reacting to [message](${msg.url}) in ${msg.channel}.`));
 
-	const restartEmbed = new Discord.MessageEmbed()
-		.setAuthor(bot.user.tag, bot.user.avatarURL())
-		.setDescription('**Restarting...**')
-		.addField('Uptime', formatAge(bot.uptime))
-		.setColor(colours.warn)
-		.setFooter(`${dtg()} | Pronto v${version}`);
-
-	dev.send(restartEmbed)
-		.then(() => process.exit())
-		.catch(error => debugError(error, `Error sending DM to ${dev}.`));
+	process.exit();
 };
--- events/exit.js
+++ events/exit.js
@@ -1,7 +1,25 @@
+const Discord = require('discord.js');
+
+const { ids: { devID }, colours } = require('../config');
+const { dtg, formatAge, sendMsg } = require('../modules');
+
 module.exports = {
 	events: [],
-	process: ['exit'],
-	execute(event, code) {
-		console.log(`Exiting with code ${code}`);
+	process: ['exit', 'SIGINT'],
+	execute() {
+		'use strict';
+
+		const { bot, version } = require('../pronto.js');
+
+		const dev = bot.users.cache.get(devID);
+
+		const restartEmbed = new Discord.MessageEmbed()
+			.setAuthor(bot.user.tag, bot.user.avatarURL())
+			.setDescription('**Restarting...**')
+			.addField('Uptime', formatAge(bot.uptime))
+			.setColor(colours.warn)
+			.setFooter(`${dtg()} | Pronto v${version}`);
+
+		sendMsg(dev, restartEmbed, true);
 	},
 };
