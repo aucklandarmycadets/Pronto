23865e3329634bbffa09aa9071a614db23049b9d
James <jqmesnz@gmail.com>
2020-10-22 13:25:50 +1300

    Iterate to v3.1.1

--- pronto.js
+++ pronto.js
@@ -12,7 +12,7 @@ Object.keys(botCommands).map(key => {
 });
 
 const TOKEN = process.env.TOKEN;
-const version = '3.1.0';
+const version = '3.1.1';
 
 bot.login(TOKEN)
 	.then(() => module.exports = {
