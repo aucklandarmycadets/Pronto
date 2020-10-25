4045f34894355944179dc01dbe9ed7d934f741be
James <jqmesnz@gmail.com>
2020-10-26 00:25:05 +1300

    Improved partial handling on message edit including cmd execution

--- events/onMessageUpdate.js
+++ events/onMessageUpdate.js
@@ -6,17 +6,19 @@ const { charLimit, dtg, sendMsg } = require('../modules');
 module.exports = {
 	events: ['messageUpdate'],
 	process: [],
-	execute(event, oldMessage, newMessage) {
+	async execute(event, oldMessage, newMessage) {
 		const { bot } = require('../pronto');
 
 		const log = bot.channels.cache.get(logID);
 		const logEmbed = new Discord.MessageEmbed()
 			.setColor(colours.warn);
 
-		if (newMessage.content.startsWith(prefix)) bot.emit('message', newMessage);
-
 		if (oldMessage.partial) {
-			logEmbed.setAuthor(newMessage.guild.name, newMessage.guild.iconURL());
+			newMessage = await newMessage.fetch();
+
+			const msgAuthor = newMessage.author;
+
+			logEmbed.setAuthor(msgAuthor.tag, msgAuthor.displayAvatarURL());
 			logEmbed.setDescription(`**Uncached message edited in ${newMessage.channel}** [Jump to Message](${newMessage.url})`);
 			logEmbed.addField('After', charLimit(newMessage.content, 1024));
 			logEmbed.setFooter(`ID: ${newMessage.id} | ${dtg()}`);
@@ -25,14 +27,16 @@ module.exports = {
 		else {
 			if (oldMessage.content === newMessage.content || newMessage.author.bot || !newMessage.guild) return;
 
-			const messageAuthor = newMessage.author;
+			const msgAuthor = newMessage.author;
 
-			logEmbed.setAuthor(messageAuthor.tag, messageAuthor.displayAvatarURL());
+			logEmbed.setAuthor(msgAuthor.tag, msgAuthor.displayAvatarURL());
 			logEmbed.setDescription(`**Message edited in ${newMessage.channel}** [Jump to Message](${newMessage.url})`);
 			logEmbed.addField('Before', charLimit(oldMessage.content, 1024));
 			logEmbed.addField('After', charLimit(newMessage.content, 1024));
-			logEmbed.setFooter(`Author: ${messageAuthor.id} | Message: ${newMessage.id} | ${dtg()}`);
+			logEmbed.setFooter(`Author: ${msgAuthor.id} | Message: ${newMessage.id} | ${dtg()}`);
 		}
+
+		if (newMessage.content.startsWith(prefix)) bot.emit('message', newMessage);
 
 		sendMsg(log, logEmbed);
 	},
