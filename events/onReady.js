1488291fcde7e887630fc7a63e80f790334b78c5
James <jqmesnz@gmail.com>
2020-10-28 00:26:58 +1300

    Don't bother checking server IDs onReady

--- events/onReady.js
+++ events/onReady.js
@@ -9,7 +9,7 @@ module.exports = {
 	process: [],
 	async execute() {
 		const { bot } = require('../pronto');
-		const { ids: { serverID }, colours } = await require('../handlers/database')();
+		const { colours } = await require('../handlers/database')();
 		const { cmds: { help } } = await require('../cmds')();
 
 		const dev = await bot.users.fetch(devID);
@@ -17,8 +17,7 @@ module.exports = {
 		console.info(`Logged in as ${bot.user.tag}!`);
 		bot.user.setActivity(`the radio net | ${await pCmd(help)}`, { type: 'LISTENING' });
 
-		if (!bot.guilds.cache.get(serverID)) return embedScaffold(dev, '**Error reaching the server, check the IDs!**', colours.error, 'dev');
-		else embedScaffold(dev, '**Ready to go!**', colours.success, 'dev');
+		embedScaffold(dev, '**Ready to go!**', colours.success, 'dev');
 
 		botPermsHandler();
 	},
