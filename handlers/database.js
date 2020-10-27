21abf96f3099b7ae1ff8d7a8453d79ce50c3f2c8
James <jqmesnz@gmail.com>
2020-10-27 21:11:24 +1300

    If message is not in guild, default to defaultServer ID

--- handlers/database.js
+++ handlers/database.js
@@ -1,10 +1,16 @@
 'use strict';
 
+const { ids: { defaultServer } } = require('../config');
+
 module.exports = async guild => {
 	const Guild = require('../models/guild');
 	const { newGuild } = require('../handlers');
 
-	return await Guild.findOne({ guildID: guild.id }, (error, found) => {
+	const id = (guild)
+		? guild.id
+		: defaultServer;
+
+	return await Guild.findOne({ guildID: id }, (error, found) => {
 		if (error) console.error(error);
 		if (!found) newGuild(guild);
 	});
