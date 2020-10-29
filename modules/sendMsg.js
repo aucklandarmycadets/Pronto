18caf904298e6eff28e8e5b10823f34dcdc4bb11
James <jqmesnz@gmail.com>
2020-10-29 19:39:21 +1300

    Import debugError only once initialised

--- modules/sendMsg.js
+++ modules/sendMsg.js
@@ -1,5 +1,7 @@
 'use strict';
 
-const { debugError } = require('./');
+module.exports = (dest, msg) => {
+	const { debugError } = require('./');
 
-module.exports = (dest, msg) => dest.send(msg).catch(error => debugError(error, `Error sending message to ${dest}.`));
+	dest.send(msg).catch(error => debugError(error, `Error sending message to ${dest}.`));
+};
