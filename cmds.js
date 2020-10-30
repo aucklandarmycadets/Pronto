d3f18aeb68f605005e794a0c5002d1fbc61cb63c
James <jqmesnz@gmail.com>
2020-10-30 22:18:25 +1300

    Fixed duplicates in commands list if no role ids

--- cmds.js
+++ cmds.js
@@ -345,6 +345,7 @@ module.exports = async guild => {
 	function equals(arr1, arr2) {
 		if (arr1 === arr2) return true;
 		if (arr1 === null || arr2 === null) return false;
+		if (!arr1.length) return false;
 		if (arr1.length !== arr2.length) return false;
 
 		for (let i = 0; i < arr1.length; i++) {
