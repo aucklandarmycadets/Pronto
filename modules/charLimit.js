f544e7db27bb6b4c6dd3bcf2409ac98fb1d085cc
James <jqmesnz@gmail.com>
2020-11-01 22:07:37 +1300

    Check for if strings are empty in charLimit to prevent empty embed fields

--- modules/charLimit.js
+++ modules/charLimit.js
@@ -1,9 +1,13 @@
 'use strict';
 
 module.exports = (str, lim) => {
-	return (str.length > lim)
+	str = (str.length > lim)
 		? (str.slice(-(str.length - lim)).includes('```'))
 			? `${str.slice(0, lim - 6)}...\`\`\``
 			: `${str.slice(0, lim - 3)}...`
 		: str;
+
+	return (str.length)
+		? str
+		: 'No message content';
 };
