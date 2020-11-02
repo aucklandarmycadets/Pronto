333b42418f20036b4b1e68191275dfa9e43fc393
James <jqmesnz@gmail.com>
2020-11-02 20:36:30 +1300

    Added lesson schema

---
+++ models/lesson.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const lessonSchema = mongoose.Schema({
+	_id: mongoose.Schema.Types.ObjectId,
+	lessonID: String,
+	lessonName: String,
+	instructors: Array,
+	dueDate: String,
+	lessonDate: String,
+	assignedResources: Array,
+	submittedResources: Array,
+});
+
+module.exports = mongoose.model('lesson', lessonSchema, 'lessons');
