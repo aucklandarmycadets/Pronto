1265df1f040255ff2c19fcd7e615a95c2379d2a6
James <jqmesnz@gmail.com>
2020-11-05 12:32:30 +1300

    Return array with removed items, instead of just the removed items

--- modules/remove.js
+++ modules/remove.js
@@ -1,8 +1,7 @@
 'use strict';
 
-module.exports = (arr, str) => {
-	const index = arr.indexOf(str);
-	return (index > -1)
-		? arr.splice(index, 1)
-		: arr;
+module.exports = (arr, str, index) => {
+	if (!index) index = arr.indexOf(str);
+	if (index > -1) arr.splice(index, 1);
+	return arr;
 };
