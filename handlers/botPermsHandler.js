9b38b449cd2471ff75d6876f61ece51441fdc70d
James <jqmesnz@gmail.com>
2020-11-15 14:07:55 +1300

    Use Set to prevent erraneous 'Permissions resolved'

--- handlers/botPermsHandler.js
+++ handlers/botPermsHandler.js
@@ -4,6 +4,8 @@ const Discord = require('discord.js');
 const { config: { permsInt } } = require('../config');
 const { embedScaffold } = require('../modules');
 
+const currentlyMissing = new Set();
+
 module.exports = async (guild, changes) => {
 	const { bot } = require('../pronto');
 	const { ids: { serverID }, colours } = await require('./database')(guild);
@@ -11,20 +13,23 @@ module.exports = async (guild, changes) => {
 	if (!serverID) return;
 
 	const requiredPermissions = new Discord.Permissions(permsInt);
-	const server = bot.guilds.cache.get(serverID);
-	const botPermissions = server.me.permissions;
+	const botPermissions = bot.guilds.cache.get(serverID).me.permissions;
 	const hasRequired = botPermissions.has(requiredPermissions);
 
 	const missingPermissions = [...new Set([...requiredPermissions].filter(value => !botPermissions.has(value)))];
 
-	if (!hasRequired) embedScaffold(guild, null, 'I do not have my minimum permissions!', colours.error, 'debug', 'Missing Permissions', missingPermissions);
+	if (!hasRequired) {
+		currentlyMissing.add(guild.id);
+		embedScaffold(guild, null, 'I do not have my minimum permissions!', colours.error, 'debug', 'Missing Permissions', missingPermissions);
+	}
 
-	else if (changes) {
+	else if (changes && currentlyMissing.has(guild.id)) {
 		const requiredArray = requiredPermissions.toArray();
 
 		for (let i = 0; i < changes.length; i++) {
 			if (requiredArray.includes(changes[i])) {
 				embedScaffold(guild, null, 'Permissions resolved.', colours.success, 'debug');
+				currentlyMissing.delete(guild.id);
 				break;
 			}
 		}
