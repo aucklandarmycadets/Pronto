dc9b4e83e6c64da916438ccc937c681810182b5d
James <jqmesnz@gmail.com>
2021-08-01 14:38:01 +1200

    Iterate version to v4.2.1

--- pronto.js
+++ pronto.js
@@ -6,7 +6,7 @@ const { mongoose } = require('./handlers');
 
 const TOKEN = process.env.TOKEN;
 const MONGOURI = process.env.MONGOURI;
-exports.version = '4.2.0';
+exports.version = '4.2.1';
 
 const bot = new Discord.Client({ partials: ['MESSAGE'] });
 
