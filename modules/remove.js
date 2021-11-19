6fd2f1eb299dd29c8f3cb272490fe246d5f3310c
James <jqmesnz@gmail.com>
2021-11-19 15:25:14 +1300

    Refactored check for undefined index to better describe intention
    Using the abstract equality operator == instead of strict equality === to support both <undefined> and <null>

--- modules/remove.js
+++ modules/remove.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (arr, str, index) => {
-	if (!index && index !== 0) index = arr.indexOf(str);
+	if (index == undefined) index = arr.indexOf(str);
 	if (index > -1) arr.splice(index, 1);
 	return arr;
 };
