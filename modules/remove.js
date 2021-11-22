3774c68683cc8d600b4aa70a30c9ab7742328a78
James <jqmesnz@gmail.com>
2021-11-22 21:55:56 +1300

    Use !== rather than > relational operator for communication of intent

--- modules/remove.js
+++ modules/remove.js
@@ -2,6 +2,6 @@
 
 module.exports = (arr, str, index) => {
 	if (index == undefined) index = arr.indexOf(str);
-	if (index > -1) arr.splice(index, 1);
+	if (index !== -1) arr.splice(index, 1);
 	return arr;
 };
