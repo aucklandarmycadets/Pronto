c7f1a13f94e2b2df30b19dd4d29775043ce8841c
James <jqmesnz@gmail.com>
2021-11-29 14:46:23 +1300

    Reaction partials are not allowed to be emitted, so there is no reason to fetch

--- events/onReaction.js
+++ events/onReaction.js
@@ -7,7 +7,6 @@ module.exports = {
 	process: [],
 	async handler(_, reaction, user) {
 		if (user.bot) return;
-		if (reaction.partial) await reaction.fetch();
 
 		manageAttendance(reaction, user);
 		updateUnsubmitted(reaction, user);
