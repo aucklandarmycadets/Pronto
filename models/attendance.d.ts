be9984a694d6a19f8f988e3b0d7e6da8a6aaa729
James <jqmesnz@gmail.com>
2021-12-02 22:20:31 +1300

    Create Typescript declaration file for <Attendance> document

---
+++ models/attendance.d.ts
@@ -0,0 +1,25 @@
+import mongoose = require('mongoose');
+import Discord = require('discord.js');
+
+/**
+ * An \<Object> representing the values of a \<mongoose.Document> to ensure attendance registers are only manageable by authorised users
+ */
+export interface Attendance extends mongoose.Document {
+	/** A unique document identifier */
+	_id: mongoose.Schema.Types.ObjectId;
+
+	/** The \<Message>.id of the \<Message> sent to the original submission channel */
+	channelID: Discord.Snowflake;
+
+	/** The \<Message>.id of the \<Message> sent to the guild's attendance channel */
+	attendanceID: Discord.Snowflake;
+
+	/** The title of this attendance register */
+	name: string;
+
+	/** The name of the formation this register belongs to */
+	formation: string;
+
+	/** A \<User>.id[] of users that have contributed to this register */
+	authors: Discord.Snowflake[];
+}
---
+++ typings.d.ts
@@ -0,0 +1,7 @@
+import { Attendance } from './models/attendance';
+import { Guild } from './models/guild';
+import { Lesson } from './models/lesson';
+
+declare global {
+	export { Attendance } from './models/attendance';
+}
