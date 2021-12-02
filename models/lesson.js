d8fd3370609c310f7561098e37eaf5c3047108d5
James <jqmesnz@gmail.com>
2021-12-02 22:20:47 +1300

    Update JSDoc for models

--- models/lesson.js
+++ models/lesson.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const mongoose = require('mongoose');
 
 /**
- * @typedef {Object} models.Lesson An \<Object> to
+ * @typedef {Object} models.Lesson An \<Object> representing the values of a \<mongoose.Document> to
  * @property {mongoose.Schema.Types.ObjectId} _id A unique document identifier
  * @property {Discord.Snowflake}
  */
