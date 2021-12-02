08d7be8598bf04ee572a543252e530fe7a263c57
James <jqmesnz@gmail.com>
2021-12-02 23:14:22 +1300

    Remove whitespaces

--- models/attendance.d.ts
+++ models/attendance.d.ts
@@ -7,19 +7,14 @@ import Discord = require('discord.js');
 export interface Attendance extends mongoose.Document {
 	/** A unique document identifier */
 	_id: mongoose.Schema.Types.ObjectId;
-
 	/** The \<Message>.id of the \<Message> sent to the original submission channel */
 	channelID: Discord.Snowflake;
-
 	/** The \<Message>.id of the \<Message> sent to the guild's attendance channel */
 	attendanceID: Discord.Snowflake;
-
 	/** The title of this attendance register */
 	name: string;
-
 	/** The name of the formation this register belongs to */
 	formation: string;
-
 	/** A \<User>.id[] of users that have contributed to this register */
 	authors: Discord.Snowflake[];
 }
