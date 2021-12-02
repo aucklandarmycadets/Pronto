6e4ac749f70ba72e21ce9db547031b8b59a84276
James <jqmesnz@gmail.com>
2021-12-02 13:56:13 +1300

    Restore processIndex for events\index.js only

--- events/index.js
+++ events/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
-const processIndex = require('../handlers/processIndex');
-
+// Load the events folder with handlers.processIndex()
 module.exports = processIndex('./events');
+
+function processIndex(directory) {
+	const fs = require('fs');
+
+	const files = fs.readdirSync(directory).filter(file => file.endsWith('.js') && file !== 'index.js');
+
+	return Object.fromEntries(
+		files.map(file => {
+			const moduleName = file.replace('.js', '');
+			return [moduleName, require(`.${directory}/${moduleName}`)];
+		}),
+	);
+}
