b0d8fcdcb30fbce12644c261fc6cd3e2bee36597
James <jqmesnz@gmail.com>
2021-12-05 15:17:11 +1300

    Add JSDoc and general comments

--- handlers/database.js
+++ handlers/database.js
@@ -10,10 +10,14 @@ const { Guild } = require('../models');
 const { merge } = require('../modules');
 
 /**
- *
- * @param {?Discord.Guild} guild
- * @param {*} changes
- * @returns {Promise<Typings.Guild>}
+ * - `handlers.database()` queries the MongoDB database for the guild's \<Guild> document if it exists, or will first call `handlers.createGuild()`
+ * to create the \<Guild> document
+ * - If the guild is `null`, the document of Pronto's 'master' guild is returned
+ * @function handlers.database
+ * @param {?Discord.Guild} guild The guild to find the \<Guild> document for
+ * - If `null`, the \<Guild> document of the default guild defined by `config.ids.DEFAULT_GUILD` will be returned instead
+ * @param {Partial<Typings.Guild>} changes A Partial\<Guild> object of the values to update within the \<Guild> document
+ * @returns {Promise<Typings.Guild>} The guild's \<Guild> document, or the `config.ids.DEFAULT_GUILD`'s \<Guild> document
  */
 module.exports = async (guild, changes) => {
 	const { bot } = await require('../pronto');
@@ -23,12 +27,14 @@ module.exports = async (guild, changes) => {
 	guild = guild || bot.guilds.cache.get(DEFAULT_GUILD);
 
 	/**
+	 * Attempt to find the \<Guild> document by querying for the guild's identifier
 	 * @type {Typings.Guild}
 	 */
 	let document = await Guild.findOne({ guildID: guild.id }, error => {
 		if (error) console.error(error);
 	});
 
+	// If there are no changes to upsert, return the document if it was found, or create (and return) a new document for the guild by calling handlers.createGuild()
 	if (!changes) return document || await createGuild(guild);
 
 	else if (changes instanceof Object) {
