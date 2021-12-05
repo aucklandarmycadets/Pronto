658b240f1d1255ac6f6db718e0a9a31ad4a1d213
James <jqmesnz@gmail.com>
2021-12-05 15:15:55 +1300

    Minor refactor of if statements for better clarity of implementation and intention

--- handlers/database.js
+++ handlers/database.js
@@ -31,8 +31,14 @@ module.exports = async (guild, changes) => {
 
 	if (!changes) return database || await createGuild(guild);
 
-	if (changes instanceof Object) database = merge(database, changes);
-	Object.keys(changes).forEach(key => database.markModified(key));
+	else if (changes instanceof Object) {
+		// Otherwise, if there are changes to upsert, call modules.merge() to merge the two objects
+		database = merge(database, changes);
 
+		// Iterate through each changed key and ensure the path is marked as having been changed
+		Object.keys(changes).forEach(key => database.markModified(key));
+	}
+
+	// Save any changes to the <Guild> and return it
 	return await database.save().catch(error => console.error(error));
 };
