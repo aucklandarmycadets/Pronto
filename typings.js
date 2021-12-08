0008f7ac6a290a8eee7820d59ae9161b30326a41
James <jqmesnz@gmail.com>
2021-12-09 12:18:37 +1300

    Create dummy typings.js module

---
+++ typings.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * This is a dummy file such that ./typings is a valid \<NodeRequire> path,
+ * as require('./typings') is being used to import typings.d.ts for VSCode IntelliSense
+ */
+module.exports = {};
