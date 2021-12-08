851d6a9470ea959bfe3421e77465edfa2c1d883e
James <jqmesnz@gmail.com>
2021-12-08 15:03:00 +1300

    Refactor into single line, and add JSDoc namespace

--- modules/updatedPermissions.js
+++ modules/updatedPermissions.js
@@ -2,15 +2,14 @@
 
 // eslint-disable-next-line no-unused-vars
 const Discord = require('discord.js');
+const { difference } = require('../modules');
 
 /**
  * Find the difference between the \<Permissions> of two \<GuildMember | Role> and return it as a \<PermissionString[]>
+ * @function modules.updatedPermissions
  * @param {Discord.GuildMember | Discord.Role} stateOne The old \<GuildMember | Role>
  * @param {Discord.GuildMember | Discord.Role} stateTwo The new \<GuildMember | Role>
  * @returns {Discord.PermissionString[]} The \<PermissionString[]> of the permissions that differ
  */
-module.exports = (stateOne, stateTwo) => {
-	const { difference } = require('./');
-	// Return the difference between the <PermissionString[]> of the old <GuildMember | Role> and the new <GuildMember | Role>
-	return difference(stateOne.permissions.toArray(), stateTwo.permissions.toArray());
-};
+// Return the difference between the <PermissionString[]> of the old <GuildMember | Role> and the new <GuildMember | Role>
+module.exports = (stateOne, stateTwo) => difference(stateOne.permissions.toArray(), stateTwo.permissions.toArray());
