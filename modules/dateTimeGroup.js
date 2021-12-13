e702c0c14fcbb9b934753c4a5bb0d6eec9585b72
James <jqmesnz@gmail.com>
2021-12-13 20:29:16 +1300

    Explicitly define default value as Date.now()

--- modules/dateTimeGroup.js
+++ modules/dateTimeGroup.js
@@ -13,13 +13,13 @@ const { findGuildConfiguration } = require('../handlers');
  * modules.dateTimeGroup(1632216060000);
  *
  * @function modules.dateTimeGroup
- * @param {string | number | Date} [date] A Date object, or a [resolvable date]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#parameters} accepted by the `Date()` constructor, such as:
+ * @param {string | number | Date} [date=Date.now()] A Date object, or a [resolvable date]{@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#parameters} accepted by the `Date()` constructor, such as:
  * - A Unix timestamp as a \<string> | \<number>
  * - A string such as `09:00 1 Aug 2021`
  * - If `undefined`, the current date will be used
  * @returns {Promise<string>} The date-time group, formatted with the [`<GuildConfiguration.settings.longDate>`]{@link models.GuildConfiguration} of the default guild
  */
-module.exports = async date => {
+module.exports = async (date = Date.now()) => {
 	const { settings: { longDate } } = await findGuildConfiguration();
 	// Format the date-time group using dateformat and return it
 	return dateFormat(date, longDate);
