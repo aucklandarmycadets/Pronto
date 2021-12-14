3ee1f26f97426e4db928dd843d6257124a71a778
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-14 14:43:35 +1300

    build(standard-version): :wrench: add updater for pronto.js

---
+++ build/standard-version-updater.js
@@ -0,0 +1,16 @@
+/**
+ * Read the current semantic version string from a file's contents
+ * @ignore
+ * @param {string} contents The contents of the file to bump
+ * @returns {string} The current semantic version string
+ */
+module.exports.readVersion = contents => /version: '(\d+\.\d+\.\d+)'/.exec(contents)[1];
+
+/**
+ * Write the bumped semantic version string to a file's contents
+ * @ignore
+ * @param {string} contents The contents of the file to bump
+ * @param {string} version The bumped semantic version string
+ * @returns {string} The file contents with the bumped version
+ */
+module.exports.writeVersion = (contents, version) => contents.replace(/(version: ')\d+.\d+.\d+'/, `$1${version}'`);
