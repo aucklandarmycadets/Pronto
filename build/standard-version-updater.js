7359227e4b0186bd76d4e446d4138806ffb8fb59
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-15 00:53:08 +1300

    build(standard-version): :hammer: bump README.md too

--- build/standard-version-updater.js
+++ build/standard-version-updater.js
@@ -4,7 +4,7 @@
  * @param {string} contents The contents of the file to bump
  * @returns {string} The current semantic version string
  */
-module.exports.readVersion = contents => /version: '(\d+\.\d+\.\d+)'/.exec(contents)[1];
+module.exports.readVersion = contents => /(?:version: '|# Pronto v)(\d+\.\d+\.\d+)/.exec(contents)[1];
 
 /**
  * Write the bumped semantic version string to a file's contents
@@ -13,4 +13,4 @@ module.exports.readVersion = contents => /version: '(\d+\.\d+\.\d+)'/.exec(conte
  * @param {string} version The bumped semantic version string
  * @returns {string} The file contents with the bumped version
  */
-module.exports.writeVersion = (contents, version) => contents.replace(/(version: ')\d+.\d+.\d+'/, `$1${version}'`);
+module.exports.writeVersion = (contents, version) => contents.replace(/(version: '|# Pronto v)\d+.\d+.\d+/, `$1${version}`);
